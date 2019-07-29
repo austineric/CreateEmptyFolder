c4fa1d307fc98701b18f35b0925f9b43d5c563da
Eric Austin <eaustin@gnresound.com>
2019-07-29 16:30:52 -0500

    CreateEmptyFolder.ps1 - Formalized

--- CreateEmptyFolder.ps1
+++ CreateEmptyFolder.ps1
@@ -1,12 +1,32 @@
 
-Do {
-    [string]$RandomText=(New-Guid)
-    $RandomText=$RandomText.Substring(0,7)
-    $ParentDirectory=Join-Path $env:USERPROFILE "\Desktop"
-    $NewDirectoryLocation=Join-Path $ParentDirectory $RandomText
-    Write-Host $NewDirectoryLocation
-} Until ((Test-Path -Path $NewDirectoryLocation) -eq $false )
 
-New-Item -ItemType "Directory" -Path $NewDirectoryLocation | Out-Null
-explorer.exe $NewDirectoryLocation
+####################################
+# Author:      Eric Austin
+# Create date: July 2019
+# Description: Quickly create a randomly-named empty folder on the desktop for temporary use
+####################################
+
+Try {
+    
+    #add the first eight characters of a GUID to the user's desktop path
+    Do {
+        [string]$RandomText=(New-Guid)
+        $RandomText=$RandomText.Substring(0,7)
+        $ParentDirectory=Join-Path $env:USERPROFILE "\Desktop"
+        $NewDirectoryLocation=Join-Path $ParentDirectory $RandomText
+        Write-Host $NewDirectoryLocation
+    } Until ((Test-Path -Path $NewDirectoryLocation) -eq $false )
+
+    #create directory and open in explorer
+    New-Item -ItemType "Directory" -Path $NewDirectoryLocation | Out-Null
+    explorer.exe $NewDirectoryLocation
+
+}
+
+Catch {
+
+    Throw $Error[0]
+    
+}
+
 
