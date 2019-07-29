ef5c66690ee041dc74e41e9776598458df3d4680
Eric Austin <eaustin@gnresound.com>
2019-07-29 16:32:30 -0500

    CreateEmptyFolder.ps1 - removed Write-Host

--- CreateEmptyFolder.ps1
+++ CreateEmptyFolder.ps1
@@ -14,7 +14,6 @@ Try {
         $RandomText=$RandomText.Substring(0,7)
         $ParentDirectory=Join-Path $env:USERPROFILE "\Desktop"
         $NewDirectoryLocation=Join-Path $ParentDirectory $RandomText
-        Write-Host $NewDirectoryLocation
     } Until ((Test-Path -Path $NewDirectoryLocation) -eq $false )
 
     #create directory and open in explorer
