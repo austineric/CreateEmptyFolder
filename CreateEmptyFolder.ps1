dec5032b4d3959fce1162cb0e4ec62b7a01cd0f5
Eric Austin <eaustin@gnresound.com>
2019-09-18 09:30:31 -0500

    Changed created folder name from using a timestamp to an instance count

--- CreateEmptyFolder.ps1
+++ CreateEmptyFolder.ps1
@@ -3,14 +3,30 @@
 ####################################
 # Author:      Eric Austin
 # Create date: July 2019
-# Description: Quickly create a randomly-named empty folder on the desktop for temporary use
+# Description: Quickly create an empty folder on the desktop for temporary use
 ####################################
+
+$FolderName=""
+$Instance=1 #initialize as 1 so the end result will always be one higher the the current number of instances
+$ParentDirectory=""
+$NewDirectoryLocation=""
 
 Try {
     
-    #set directory name as a datetime
+    $FolderName=(Get-Date).ToString("yyyy.MM.dd_")
     $ParentDirectory=Join-Path $env:USERPROFILE "\Desktop"
-    $NewDirectoryLocation=Join-Path $ParentDirectory (Get-Date).ToString("yyyy.MM.ddTHH.mm.ss")
+
+    Get-ChildItem -Path $ParentDirectory | ForEach-Object {
+        if ($_.Name -like $FolderName + "*" ){
+            $Instance=$Instance + 1
+        }
+    }
+
+    #set final folder name
+    $FolderName=$FolderName + $Instance.ToString()
+
+    #set final new directory location
+    $NewDirectoryLocation=Join-Path $ParentDirectory $FolderName
 
     #create directory and open in explorer
     New-Item -ItemType "Directory" -Path $NewDirectoryLocation | Out-Null
